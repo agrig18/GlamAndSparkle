993c9599db021a353dbbbb8b83f83045f3ad9b47
agrig18 <agrig18@freeuni.edu.ge>
2020-11-19 17:48:04 +0400

    add event listener to overlay

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
     let searchIcon = document.querySelector('.btn.icon');
+    let overlay = document.getElementById('search-overlay');
+    let overlay_dimmer = document.getElementById('search-overlay-dimmer');
     searchIcon.addEventListener('click',function(event){
-        let overlay = document.getElementById('search-overlay');
         overlay.style.display = 'block';
-        let overlay_dimmer = document.getElementById('search-overlay-dimmer');
         overlay_dimmer.style.display = 'block'
         event.preventDefault();
     });
+
+    function fadeOutEffect() {
+        overlay.style.display = 'none';
+        overlay_dimmer.style.display = 'none'
+        event.preventDefault();
+    }
+    
+    overlay.addEventListener('mouseleave', fadeOutEffect);
 })
