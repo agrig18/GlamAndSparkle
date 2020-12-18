617f696fe9d6e0abb90d605fda999b3214753064
agrig18 <agrig18@freeuni.edu.ge>
2020-12-19 00:31:33 +0400

    add search overlay dimmer

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
     let searchIcon = document.querySelector('.btn.icon');
     let overlay = document.getElementById('search-overlay');
-    let overlay_dimmer = document.getElementById('search-overlay-dimmer');
+    let search_overlay_dimmer = document.getElementById('search-overlay-dimmer');
     searchIcon.addEventListener('click',function(event){
         overlay.style.display = 'block';
-        overlay_dimmer.style.display = 'block'
+        search_overlay_dimmer.style.display = 'block'
         event.preventDefault();
     });
 
     function fadeOutEffect() {
         overlay.style.display = 'none';
-        overlay_dimmer.style.display = 'none'
+        search_overlay_dimmer.style.display = 'none'
         event.preventDefault();
     }
     
